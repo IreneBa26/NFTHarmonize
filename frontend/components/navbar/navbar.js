c054ad2dca7b853a206662f7b11afad55adfec3b
Valerie Vaske <32402989+VMLVaske@users.noreply.github.com>
2023-08-12 23:39:17 +0200

    i have no clue what i am doingg

---
+++ frontend/components/navbar/navbar.js
@@ -0,0 +1,90 @@
+import { ThemeChanger } from "../navbar/themeChanger";
+import {
+    Button,
+    Grid,
+    Spacer,
+    Text,
+    Container,
+    Row,
+    Col,
+    Link
+} from "@nextui-org/react";
+import Logo from "../../public/agronomy.png";
+import Image from 'next/image'
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+import {
+    useAddress,
+    useDisconnect,
+    useMetamask,
+    useNetworkMismatch,
+    useNetwork,
+} from "@thirdweb-dev/react";
+
+import useAuthenticate from "../../hooks/useAuthenticate";
+import truncateEthAddress from "truncate-eth-address";
+
+export default function Navbar() {
+    const router = useRouter();
+
+    const address = useAddress();
+    const disconnect = useDisconnect();
+    const connectWithMetamask = useMetamask();
+    const { login, authenticate, logout } = useAuthenticate();
+    const isMitsmatched = useNetworkMismatch();
+    const [, switchNetwork] = useNetwork();
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [authMessage, setAuthMessage] = useState("N/A");
+
+    return (
+        <Grid.Container gap={2} justify="center" alignItems="center">
+            <Spacer />
+            <Grid>
+                <Image
+                    src={Logo}
+                    alt="bread>beer>bread"
+                    objectFit="cover"
+                    width={64}
+                    height={64}
+                />
+
+            </Grid>
+            <Grid>
+                <Button onPress={() => router.push("/")}>Home</Button>
+            </Grid>
+            <Grid>
+                <Button onPress={() => router.push("/marketplace")}>Market</Button>
+            </Grid>
+            <Grid>
+                <Button onPress={() => router.push("/collection")}>Collection</Button>
+            </Grid>
+            <Grid>
+                <Button onPress={() => router.push("/about")}>About</Button>
+            </Grid>
+            <Grid>
+                <ThemeChanger />
+            </Grid>
+            <Grid>
+                {address ? (
+                    <Container>
+                        <Row>
+                            <Button flat auto onPress={disconnect}>
+                                Disconnect Wallet
+                            </Button>
+                        </Row>
+                        <Row>
+                            <Text>Address: {truncateEthAddress(address)}</Text>
+                        </Row>
+                    </Container>
+                ) : (
+                    <Button flat auto onPress={connectWithMetamask}>
+                        Connect Wallet
+                    </Button>
+                )}
+            </Grid>
+
+        </Grid.Container>
+    );
+}
---
+++ frontend/pages/collection.js
@@ -0,0 +1,35 @@
+import Layout from '../components/layout'
+import { Container, Spacer, Divider, Text, Row, Col } from '@nextui-org/react'
+
+import { useAddress } from '@thirdweb-dev/react';
+import { useState } from "react";
+import OwnedNFTs from "../components/ownedNFTs";
+import CreateNFTModal from '../components/modals/createNFTModal';
+
+export default function Collection() {
+
+    const address = useAddress();
+
+    return (
+        <Layout>
+            <div>
+                <main>
+                    <Container fluid >
+                        <Row justify="center">
+                            <Text h1>NFT Collection</Text>
+                        </Row>
+                        <Spacer />
+                        <Divider />
+                        <Spacer />
+                        <Row justify="center">
+                            <OwnedNFTs />
+                        </Row>
+                        <Spacer />
+                        <Divider />
+                        <Spacer />
+                    </Container>
+                </main>
+            </div>
+        </Layout>
+    )
+}
